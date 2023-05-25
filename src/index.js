7518eb142a9c5bfd55438f3e051846e553a1ee57
Andrey Popazov <andrey.plagiatix@gmail.com>
2023-05-25 17:39:20 +0300

    Pushing working app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import Calculator from './Calculator.js';
 export { default as Untitled } from "./Calculator.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Calculator />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
